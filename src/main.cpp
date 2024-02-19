8d037cd8d802d9aeb7425d5dae0b62297dafd5cb
THdeCamargoJ <thdecamargoj@gmail.com>
2024-02-18 22:04:30 -0300

    Phase 1: Create a blank screen and the game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <raylib.h>
 
 using namespace std;
 
 int main () {
-    cout << "I'm working" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+
+    InitWindow(screen_width, screen_height, "My Pong Game");
+    
+    SetTargetFPS(60); // 60 fps - draws 60 frames per second
+
+    while(WindowShouldClose() == false) {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
