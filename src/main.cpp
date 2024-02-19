4d40aa7bdf655a2a5f4153c9197f9dbd27085102
THdeCamargoJ <thdecamargoj@gmail.com>
2024-02-18 22:23:28 -0300

    Phase 2: draw the paddles and the ball

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,41 @@ int main () {
     const int screen_width = 1280;
     const int screen_height = 800;
 
+    const int screen_offset = 10;
+
+    const int rectange_height = 120;
+    const int rectange_width = 25;
+
     InitWindow(screen_width, screen_height, "My Pong Game");
     
     SetTargetFPS(60); // 60 fps - draws 60 frames per second
 
     while(WindowShouldClose() == false) {
         BeginDrawing();
+
+        // ball
+        DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+
+        // first pad
+        DrawRectangle(
+            screen_offset,
+            screen_height / 2 - rectange_height / 2,
+            rectange_width,
+            rectange_height,
+            WHITE
+        );
+
+        // second pad
+        DrawRectangle(
+            screen_width - (rectange_width + screen_offset),
+            screen_height / 2 - rectange_height / 2,
+            rectange_width,
+            rectange_height,
+            WHITE
+        );
+
+        // middle line: separates left player from right player fields
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
 
         EndDrawing();
     }
