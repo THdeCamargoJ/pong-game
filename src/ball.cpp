4e46324ba55c4aaba584a604964f1b3c0580bd1f
THdeCamargoJ <thdecamargoj@gmail.com>
2024-02-19 00:48:51 -0300

    Phase 4: check for ball colisions with screen edges

--- src/ball.cpp
+++ src/ball.cpp
@@ -8,4 +8,10 @@ void Ball::Draw() {
 void Ball::Move() {
     Ball::x += Ball::x_speed;
     Ball::y += Ball::y_speed;
+
+    if (Ball::y + Ball::radius >= GetScreenHeight() || Ball::y - Ball::radius <= 0) {
+        Ball::y_speed *= -1;
+    } else if (Ball::x + Ball::radius >= GetScreenWidth() || Ball::x - Ball::radius <= 0) {
+        Ball::x_speed *= -1;
+    }
 }
